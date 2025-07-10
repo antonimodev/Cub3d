c0b54d696eda1b8d6214fd51a4b053bd098735e5
Antonio Luis Moreno Del Pino <antonimo@c3r5s6.42malaga.com>
2025-07-10 14:27:28 +0200

    update movement

--- src/hooks/movement.c
+++ src/hooks/movement.c
@@ -6,7 +6,7 @@
 /*   By: antonimo <antonimo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 10:52:53 by antonimo          #+#    #+#             */
-/*   Updated: 2025/07/10 14:05:24 by antonimo         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:27:09 by antonimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,24 +67,24 @@ void	move_right(t_game *cub3d)
 
 void move_forward(t_game *cub3d)
 {
-    cub3d->player.x += cub3d->player.angle.cos_angle * PLAYER_SPEED;
-    cub3d->player.y += cub3d->player.angle.sin_angle * PLAYER_SPEED;
+	cub3d->player.x += cub3d->player.angle.cos_angle * PLAYER_SPEED;
+	cub3d->player.y += cub3d->player.angle.sin_angle * PLAYER_SPEED;
 }
 
 void move_back(t_game *cub3d)
 {
-    cub3d->player.x -= cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← MENOS
-    cub3d->player.y -= cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← MENOS
+	cub3d->player.x -= cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← MENOS
+	cub3d->player.y -= cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← MENOS
 }
 
 void move_left(t_game *cub3d)  // Movimiento lateral (perpendicular)
 {
-    cub3d->player.x += cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← sin
-    cub3d->player.y -= cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← cos
+	cub3d->player.x += cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← sin
+	cub3d->player.y -= cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← cos
 }
 
 void move_right(t_game *cub3d)  // Movimiento lateral (perpendicular)
 {
-    cub3d->player.x -= cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← -sin
-    cub3d->player.y += cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← cos
+	cub3d->player.x -= cub3d->player.angle.sin_angle * PLAYER_SPEED;  // ← -sin
+	cub3d->player.y += cub3d->player.angle.cos_angle * PLAYER_SPEED;  // ← cos
 }
